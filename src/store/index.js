736d571573da6073f2b6fda854ff9ee64ba40ee5
Paulo Sergio <pauloserg861@gmail.com>
2022-09-24 11:29:20 -0300

    criando estrutura para estados globais

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import vuex from 'vuex'
+import Service from '../services/axios-requests'
+
+Vue.use(vuex)
+
+export default new vuex.Store({
+  state: {},
+  mutations: {},
+  actions: {},
+  modules:{}
+})
