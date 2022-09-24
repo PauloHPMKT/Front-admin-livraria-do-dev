136acfc8d1cbf1da5b0ab89430cf1a3c49b876cb
Paulo Sergio <pauloserg861@gmail.com>
2022-09-24 13:40:53 -0300

    criando estado global para armazenar dados do login

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,30 @@ import Service from '../services/axios-requests'
 Vue.use(vuex)
 
 export default new vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    user: {
+      name: '',
+      email: '',
+      password: '',
+    },
+    access_token: '',
+  },
+  mutations: {
+    authLogin(state, payload) {
+      state.access_token = payload.access_token
+      state.user = payload.user
+    }
+  },
+  actions: {
+    async handleSubmitLogin( { commit }, user ) {
+      await Service.login({
+        email: user.email,
+        password: user.password
+      }).then(res => res.data)
+
+      commit('authLogin', res.data)
+    }
+
+  },
   modules:{}
 })
