d703e5c1cfe0afd3f52549e5ec3fc32f93737a2b
Paulo Sergio <pauloserg861@gmail.com>
2022-09-26 20:39:51 -0300

    criando pagina 404

--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -1,66 +1,44 @@
 <template>
-  <div class="contact-us full-screen">
-    <nav class="navbar navbar-ct-default" role="navigation-demo">
-      <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example-2">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <router-link :to="{path:'/'}" class="navbar-brand">Site title</router-link>
-        </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="navigation-example-2">
-          <ul class="nav navbar-nav navbar-right">
-            <li>
-              <router-link :to="{path:'/'}">Home</router-link>
-            </li>
-          </ul>
-        </div>
-        <!-- /.navbar-collapse -->
-      </div>
-      <!-- /.container-->
-    </nav>
-    <div class="wrapper wrapper-full-page section content">
-      <div class="">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-8 col-md-offset-2 text-center">
-              <h2 class="title text-danger">404 Not Found</h2>
-              <h2 class="title">Oops! It seems that this page does not exist.</h2>
-            </div>
-          </div>
-        </div>
-      </div>
+  <div class="error-page">
+    <div>
+      <img src="/img/robot-error-removebg.png" alt="robo error">
     </div>
-    <footer class="footer-demo">
-      <div class="container">
-        <nav class="pull-left">
-          <ul>
-            <li>
-              <router-link :to="{path:'/'}">Home</router-link>
-            </li>
-            <li>
-              <router-link :to="{path:'/register'}">Register</router-link>
-            </li>
-          </ul>
-        </nav>
-        <div class="copyright text-center">
-          &copy; Coded with
-          <i class="fa fa-heart heart"></i> by
-          <a href="https://binarcode.com" target="_blank">BinarCode</a>.
-          Designed by <a href="https://www.creative-tim.com/?ref=pdf-vuejs" target="_blank">Creative Tim</a>.
-        </div>
-      </div>
-    </footer>
+    <div class="align-text">
+      <h2>Página não encontrada</h2>
+      <p>Desculpe, nós não conseguimos a página que voce está procurando. <span>Sugerimos que retorne para a sessão de login.</span></p>
+
+      <router-link to="/login">Vá para o login</router-link>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {}
+export default {
+  name: 'NotFoundPage'
+}
 
 </script>
+<style scoped>
+.error-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+}
+.error-page img {
+  filter: drop-shadow(-2px 2px 3px #00445a91)
+}
+
+.error-page .align-text {
+  text-align: center;
+}
+
+.error-page span {
+  display: block;
+}
+
+a {
+  padding: 10px;
+}
+</style>
