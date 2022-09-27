f928b9dad6c7bcfa67f288cfae41e90845bb48cb
Paulo Sergio <pauloserg861@gmail.com>
2022-09-26 22:38:15 -0300

    organizando estruturas

--- src/pages/TableList.vue
+++ src/pages/TableList.vue
@@ -3,16 +3,18 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-12">
-          <card class="strpied-tabled-with-hover"
-                body-classes="table-full-width table-responsive"
+          <card 
+            class="strpied-tabled-with-hover"
+            body-classes="table-full-width table-responsive"
           >
             <template slot="header">
               <h4 class="card-title">Striped Table with Hover</h4>
               <p class="card-category">Here is a subtitle for this table</p>
             </template>
-            <l-table class="table-hover table-striped"
-                     :columns="table1.columns"
-                     :data="table1.data">
+            <l-table 
+              class="table-hover table-striped"
+              :columns="table1.columns"
+              :data="table1.data">
             </l-table>
           </card>
 
@@ -25,25 +27,28 @@
               <p class="card-category">Here is a subtitle for this table</p>
             </template>
             <div class="table-responsive">
-              <l-table class="table-hover"
-                       :columns="table2.columns"
-                       :data="table2.data">
+              <l-table 
+                class="table-hover"
+                :columns="table2.columns"
+                :data="table2.data">
               </l-table>
             </div>
           </card>
         </div>
 
         <div class="col-12">
-          <card class="strpied-tabled-with-hover"
-                body-classes="table-full-width table-responsive"
+          <card 
+            class="strpied-tabled-with-hover"
+            body-classes="table-full-width table-responsive"
           >
             <template slot="header">
               <h4 class="card-title">Small table</h4>
               <p class="card-category">Here is a subtitle for this table</p>
             </template>
-            <l-table class="table-hover table-striped table-sm"
-                     :columns="table1.columns"
-                     :data="table1.data">
+            <l-table 
+              class="table-hover table-striped table-sm"
+              :columns="table1.columns"
+              :data="table1.data">
             </l-table>
           </card>
 
