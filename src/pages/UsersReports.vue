0fe8adac739621cedef92456d99b026bac147c82
Paulo Sergio <pauloserg861@gmail.com>
2022-09-26 22:36:17 -0300

    implementando tabela de usuarios

--- src/pages/UsersReports.vue
+++ src/pages/UsersReports.vue
@@ -3,153 +3,66 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-12">
-          <card class="strpied-tabled-with-hover"
-                body-classes="table-full-width table-responsive"
+          <card 
+            class="strpied-tabled-with-hover"
+            body-classes="table-full-width table-responsive"
           >
             <template slot="header">
-              <h4 class="card-title">Striped Table with Hover</h4>
-              <p class="card-category">Here is a subtitle for this table</p>
+              <h4 class="card-title">Relatório de usuários</h4>
+              <p class="card-category">Usuários disponíveis na plataforma</p>
             </template>
-            <l-table class="table-hover table-striped"
-                     :columns="table1.columns"
-                     :data="table1.data">
-            </l-table>
-          </card>
-
-        </div>
-
-        <div class="col-12">
-          <card class="card-plain">
-            <template slot="header">
-              <h4 class="card-title">Table on Plain Background</h4>
-              <p class="card-category">Here is a subtitle for this table</p>
-            </template>
-            <div class="table-responsive">
-              <l-table class="table-hover"
-                       :columns="table2.columns"
-                       :data="table2.data">
-              </l-table>
-            </div>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th 
+                    v-for="(column, index) in columns" 
+                    :key="index">{{ column }}
+                  </th>
+                </tr>
+              </thead>
+              <tbody v-for="user in users" :key="user._id">
+                <tr>
+                  <td>{{ user.name }}</td>
+                  <td>{{ user.email }}</td>
+                </tr>
+              </tbody>
+            </table>
           </card>
         </div>
-
-        <div class="col-12">
-          <card class="strpied-tabled-with-hover"
-                body-classes="table-full-width table-responsive"
-          >
-            <template slot="header">
-              <h4 class="card-title">Small table</h4>
-              <p class="card-category">Here is a subtitle for this table</p>
-            </template>
-            <l-table class="table-hover table-striped table-sm"
-                     :columns="table1.columns"
-                     :data="table1.data">
-            </l-table>
-          </card>
-
-        </div>
-
       </div>
     </div>
   </div>
 </template>
 <script>
-  import LTable from 'src/components/Table.vue'
-  import Card from 'src/components/Cards/Card.vue'
-  const tableColumns = ['Id', 'Name', 'Salary', 'Country', 'City']
-  const tableData = [{
-    id: 1,
-    name: 'Dakota Rice',
-    salary: '$36.738',
-    country: 'Niger',
-    city: 'Oud-Turnhout'
-  },
-  {
-    id: 2,
-    name: 'Minerva Hooper',
-    salary: '$23,789',
-    country: 'Curaçao',
-    city: 'Sinaai-Waas'
-  },
-  {
-    id: 3,
-    name: 'Sage Rodriguez',
-    salary: '$56,142',
-    country: 'Netherlands',
-    city: 'Baileux'
-  },
-  {
-    id: 4,
-    name: 'Philip Chaney',
-    salary: '$38,735',
-    country: 'Korea, South',
-    city: 'Overland Park'
-  },
-  {
-    id: 5,
-    name: 'Doris Greene',
-    salary: '$63,542',
-    country: 'Malawi',
-    city: 'Feldkirchen in Kärnten'
-  }]
-  export default {
-    components: {
-      LTable,
-      Card
-    },
-    data () {
-      return {
-        table1: {
-          columns: [...tableColumns],
-          data: [...tableData]
-        },
-        table2: {
-          columns: [...tableColumns],
-          data: [...tableData]
-        }
-      }
-    }
-  }
-</script>
-<style>
-</style>
-
-
-<!--<template>
-  <div>
-    <h2>Teste de exibicao de usuarios</h2>
-    <ul>
-      <li v-for="user in users" :key="user._id">{{ user.name }} - {{ user.email }}</li>
-    </ul>
-    <!--<ul>
-      <li v-for="user in users" :key="user._id">{{ user.name }}</li>
-    </ul>
-  </div>
-</template>
-
-<script>
+import LTable from 'src/components/Table.vue'
+import Card from 'src/components/Cards/Card.vue'
 import Service from '../services/axios-requests'
+const tableColumns = ['Nome', 'E-mail']
+
 export default {
-  name: 'UsersReports',
-  data() {
+  components: {
+    LTable,
+    Card
+  },
+  data () {
     return {
+      columns: [...tableColumns],
       users: [],
     }
   },
-
   methods: {
-    async listUsers() {
-      await Service.listar().then(res => {
-        this.users = res.data
+    listUsers() {
+      Service.listar().then(res => {
+        const dataParse = JSON.parse(JSON.stringify(res.data))
+        this.users = dataParse
       })
     }
   },
+
   mounted() {
     this.listUsers()
   }
 }
 </script>
-
-<style lang="scss" scoped>
-  
-</style>-->
+<style>
+</style>
