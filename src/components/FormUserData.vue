bfd51f634f9094a8b6943c27cb946e44424be259
Paulo Sergio <pauloserg861@gmail.com>
2022-09-28 23:19:01 -0300

    criando estrutura de formulario de cadastro

---
+++ src/components/FormUserData.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="content card form-content">
+    <div>
+      <font-awesome-icon icon="fa-solid fa-user-plus" />
+    </div>
+    Form para novo usuario
+  </div>
+</template>
+<script>
+export default {
+  name: 'FormUserData'
+}
+</script>
+
+<style scoped>
+.form-content {
+  width: 400px;
+  text-align: center;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  box-shadow: -2px 2px 5px rgba(0, 0, 0, 0.342);
+  padding: 30px;
+  animation: blow .5s ease-in-out;
+}
+</style>
