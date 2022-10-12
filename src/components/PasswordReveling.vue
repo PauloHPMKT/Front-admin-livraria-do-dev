2b932835a00951ccf9b586c239907162b4fff81f
Paulo Sergio <pauloserg861@gmail.com>
2022-10-11 22:05:57 -0300

    criando componente de ocultar senha

---
+++ src/components/PasswordReveling.vue
@@ -0,0 +1,19 @@
+<template>
+  <span @click="$emit('toggle')">
+    <font-awesome-icon :icon="['fas', 'eye']" v-if="isPassword"/>
+    <font-awesome-icon :icon="['fas', 'eye-slash']" v-else/>
+  </span>
+</template>
+<script>
+export default {
+  name: 'PasswordReveling',
+  emits: ['toggle'],
+  props: {
+    isPassword: Boolean,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
