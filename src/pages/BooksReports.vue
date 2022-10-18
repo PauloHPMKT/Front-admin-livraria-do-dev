fec100cbffd525b986d4ad99f9fdc93d2cc9efc8
Paulo Sergio <pauloserg861@gmail.com>
2022-10-17 22:14:20 -0300

    criando card de livros

--- src/pages/BooksReports.vue
+++ src/pages/BooksReports.vue
@@ -18,11 +18,48 @@
             </template> 
           </card>  
           <div class="table-content card">
-            <div class="container-box card"> 
-              <ul>
-                <li v-for="book in books" :key="book._id">{{ book.title }}</li>
-              </ul>
-            </div>
+            <ul>
+              <li v-for="book in books" :key="book._id">
+                <card>
+                  <div class="book-commands">
+                    <div class="book-description">
+                      <h3>{{ book.title }}</h3>
+                      <p>{{ book.plot }}</p>
+                    </div>
+                    <div class="icon-menu">
+                      <font-awesome-icon icon="fa-solid fa-bars" />
+                    </div>
+                  </div>
+                  <div class="card-book">
+                    <div class="book-image">
+                      <img :src="book.poster" alt="imagem do livro" />
+                    </div>
+                    <div class="book-info-content">
+                      <header class="tecnichal-data">
+                        <div class="rating-book">{{ book.technical.rating }}</div>
+                        <div>Leram: <span>{{ book.technical.readers }}</span></div>
+                        <div>Votaram: <span>{{ book.technical.votes }}</span></div>
+                      </header>
+                      <article class="full_plot">
+                        <p>{{ book.full_plot }}</p>
+                      </article>
+                      <footer class="book-technical-infos">
+                        <div class="technical-infos">
+                          <p>Autor: {{ book.author.name }}</p>
+                          <p>Editora: {{ book.publishing }}</p>
+                          <p>Genero: {{ book.genres }}</p>
+                        </div>
+                        <div class="technical-infos">
+                          <p>Idioma: {{ book.language }}</p>
+                          <p>Ano de lancamento: {{ book.year }}</p>
+                          <p>Número de páginas: {{ book.pages_number }}</p>
+                        </div>
+                      </footer>
+                    </div>
+                  </div>
+                </card>
+              </li>
+            </ul>
           </div> 
         </div>
       </div>
@@ -60,11 +97,97 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.container-box {
+.book-commands {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .book-description {
+    cursor: pointer;
+  
+    h3 {
+      margin: 0;
+      font-size: 25px;
+    }
+  
+    p {
+      margin-bottom: 30px;
+      color: #808080;
+    }
+  }
+
+  .icon-menu {
+    background: #808080;
+    padding: 10px 15px;
+    border-radius: 10px;
+    cursor: pointer;
+    box-shadow: -2px 2px 5px rgba(0, 0, 0, 0.24);
+
+    svg {
+      font-size: 30px;
+      color: #fff;
+    }
+  }
+}
+
+.card-book {
+  display: flex;
+
+  .book-image {
+    width: 20%;
+
+    img {
+      width: 100%;
+    }
+  }
+
+  .book-info-content {
+    margin: 0 40px;
+    width: 80%;
+    color: #808080;
+
+    .tecnichal-data {
+      display: flex;
+      margin-bottom: 20px;
+
+      .rating-book {
+        background-color: #009acc;
+        color: #fff;
+        border-radius: 10px;
+      }
+
+      div {
+        padding: 20px;
+
+        span {
+          font-weight: 700;
+          color: #009acc;
+        }
+      }
+    }
+
+    .full_plot {
+      margin-bottom: 40px;
+    }
+
+    .book-technical-infos {
+      display: flex;
+
+      .technical-infos {
+        margin-right: 50px;
+
+        p {
+          margin: 0;
+        }
+      }
+    }
+  }
+}
+/*.container-box {
   padding: 20px 0;
 
   ul {
     padding: 0 20px;
   }
-}
+}*/
 </style>
